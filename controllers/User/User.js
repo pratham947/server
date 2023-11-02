ec64c453b97eec290b5ed58d18d19d7c8b454db2
pratham <mehtapratham49@gmail.com>
2023-11-03 03:18:16 +0530

    dfjdkf

--- controllers/User/User.js
+++ controllers/User/User.js
@@ -1,18 +1,12 @@
 import User from "../../models/UserAuth.js";
 import jwt from "jsonwebtoken";
 
-const getUserFromToken = async (token) => {
-  const { userId: Id } = jwt.verify(token, process.env.JWT_SECRET);
-  const user = await User.findById(Id);
-  if (!user) return false;
-  return user;
+export const getUser = (req, res) => {
+  const { token } = req.body;
+  console.log("called");
+  const verify = jwt.verify(token, process.env.JWT_SECRET);
+  console.log(verify);
 };
-
-export const getUser = async (req,res) =>{
-  const {token} = req.body;
-  const data= jwt.verify(token,process.env.JWT_SECRET)
-  return data;
-}
 
 export const updateUser = async (req, res) => {
   const { token } = req.body;
